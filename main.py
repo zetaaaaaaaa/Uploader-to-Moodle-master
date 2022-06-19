d8080160de1894673e71eaa06e27700dd702a4d0
zeta30 <102247888+zeta30@users.noreply.github.com>
2022-06-19 12:33:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -624,11 +624,11 @@ def onmessage(update,bot:ObigramClient):
                 #    api_id = os.environ.get('api_id')
                 #    api_hash = os.environ.get('api_hash')
                 #    bot_token = os.environ.get('bot_token')
-                #    
+                #  
                 # set in debug
-                #    api_id = 7386053
-                #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-                #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+                #    api_id = 17897679
+                #    api_hash = 'e7681747482f5867af9de8171136a89b'
+                #    bot_token = '5538378058:AAHm7FstN6-ZpkltzheqkE7yFGDM82z8Usc'
 
                 #    chat_id = int(update.message.chat.id)
                 #    message_id = int(update.message.message_id)
