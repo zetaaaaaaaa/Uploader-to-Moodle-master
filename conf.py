aa03418ef3a18c40d0509b4f0d50c31495c4719c
zeta30 <102247888+zeta30@users.noreply.github.com>
2022-06-19 12:39:56 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 import os
 
-api_id = os.environ.get('api_id')
-api_hash = os.environ.get('api_hash')
-bot_token = os.environ.get('bot_token')
-bot_admin_group = os.environ.get('bot_admin_group')
+api_id = os.environ.get('17897679')
+api_hash = os.environ.get('e7681747482f5867af9de8171136a89b')
+bot_token = os.environ.get('5538378058:AAHm7FstN6-ZpkltzheqkE7yFGDM82z8Usc')
+bot_admin_group = os.environ.get('Zeta30')
