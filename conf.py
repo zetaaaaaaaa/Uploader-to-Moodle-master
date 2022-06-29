dcb9171cb3e9cf8f97ed50131a3bc9edd2c82c08
zetaaaaaaaa <102250655+zetaaaaaaaa@users.noreply.github.com>
2022-06-29 08:32:37 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,5 +2,5 @@ import os
 
 api_id = os.environ.get('17897679')
 api_hash = os.environ.get('e7681747482f5867af9de8171136a89b')
-bot_token = os.environ.get('5538378058:AAHm7FstN6-ZpkltzheqkE7yFGDM82z8Usc')
+bot_token = os.environ.get('5355334143:AAFNAEAz2PkNzg6Z0FCrBkvBv5jp5AWYi2Q')
 bot_admin_group = os.environ.get('Zeta30')
